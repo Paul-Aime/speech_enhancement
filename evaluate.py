ac01e3ccf3df782f31c3ba41bb1561fe270b800f
Paul-Aime <paul.aime@grenoble-inp.org>
2020-01-16 04:57:07 +0100

    eval snr -10

--- evaluate.py
+++ evaluate.py
@@ -36,7 +36,7 @@ def main():
     mode = 'auto'  # Load the last trained model from params settings
 
     params = params_utils.Params()
-    params.snr = -20  # dB
+    params.snr = -10  # dB
 
     # Get the last from params settings
     if mode == 'auto':
